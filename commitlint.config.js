3c287dfe57c8a6221417147ee35d676a0107eb11
chrsm <git@c.chrsm.org>
2023-03-01 10:35:15 -0800

    do thing: thing thing

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,9 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+	extends: ['@commitlint/config-conventional']
+	rules: {
+		'type-enum': [2, 'never', []],
+	},
+	parserPreset: {
+		parserOpts: { headerPattern: '/^([^0-9]\\w*)(?:\\(([^0-9].*)\\))?!?: (.*)$/' },
+	},
+};
